f887a1fe388a08074e89a9b8ddf2086ce26825c0
Robert Damas <robert.damas@byom.de>
2016-10-07 19:41:19 +0200

    Version-String

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -2,7 +2,7 @@
 #######################################################################
 #
 #  Oscam Skyde Status
-#  Version 0.1
+#  Version 0.2
 #
 #  Copyright (c) 2016 by Robert Damas
 #  All rights reserved.
